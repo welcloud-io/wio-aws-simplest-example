9eebf24dab8690b03729c7fa4d8f253d9cf19dc2
Olivier Lemaitre <o.lemaitre@welcloud.io>
2022-02-25 19:59:06 +0000

    init repo with first ec2 template

---
+++ cloudformations-templates/publish-templates.sh
@@ -0,0 +1,23 @@
+bucketname='welcloud.io.aws-simplest-example'
+folder='ec2'
+templatename='one-ec2-instance-with-latest-ami.template.yaml'
+
+region='eu-west-1'
+readmefile='../readme.md'
+cat << EOF > $readmefile
+### Ready to use Cloudformation templates
+#### Region = eu-west-1 (Ireland)
+---
+EOF
+
+for folder in $(ls -d */); do
+for templatename in $(ls $folder); do 
+aws s3 cp $folder$templatename s3://$bucketname/cloudformation-templates/$folder$templatename
+
+stackname=$(echo $templatename |cut -d. -f1)
+cloudformationurl="https://$region.console.aws.amazon.com/cloudformation/home?region=$region#/stacks/create/review?"
+templateurl="https://s3.$region.amazonaws.com/$bucketname/cloudformation-templates/$folder$templatename"
+
+echo "[CREATE STACK $stackname]($cloudformationurl?templateURL=$templateurl&stackName=$stackname) | [download-template]($templateurl)" >> $readmefile
+done
+done
---
+++ readme.md
@@ -0,0 +1,4 @@
+### Ready to use Cloudformation templates
+#### Region = eu-west-1 (Ireland)
+---
+[CREATE STACK one-ec2-instance-with-latest-ami](https://eu-west-1.console.aws.amazon.com/cloudformation/home?region=eu-west-1#/stacks/create/review??templateURL=https://s3.eu-west-1.amazonaws.com/welcloud.io.aws-simplest-example/cloudformation-templates/ec2/one-ec2-instance-with-latest-ami.template.yaml&stackName=one-ec2-instance-with-latest-ami) | [download-template](https://s3.eu-west-1.amazonaws.com/welcloud.io.aws-simplest-example/cloudformation-templates/ec2/one-ec2-instance-with-latest-ami.template.yaml)
